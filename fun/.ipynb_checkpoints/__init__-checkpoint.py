23f873595d6224d5d67a00c17f7a5c74bd15ac3f
Wen-Han Yu <wenhan.yu@gatesmri.org>
2022-09-02 02:51:52 +0000

    Initial commit

--- fun/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,4 +0,0 @@
-#from .network_analysis import geneAssemClass as gAssem
-#from .predictive_model import predModelClass as gpmod
-#from .validation import valModelClass as gval
-#from .plotting import studyInfoClass
--- fun/R/.ipynb_checkpoints/array_normalize-checkpoint.r
+++
@@ -1,8 +0,0 @@
-library(preprocessCore)
-
-array_normalize <- function(expr){
-    re_expr <- preprocessCore::normalize.quantiles(expr)
-    colnames(re_expr)<-colnames(expr)
-    rownames(re_expr)<-rownames(expr)
-    return(re_expr)
-}
